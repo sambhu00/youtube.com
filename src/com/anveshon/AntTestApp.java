e60d98aafdac011f3796df8a004e20c074164fe8
sambhu <sambhu00@gmail.com>
2013-05-25 15:37:42 +0530

    initial commit

---
+++ src/com/anveshon/AntTestApp.java
@@ -0,0 +1,15 @@
+package com.anveshon;
+
+public class AntTestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello shiv");
+			Calculate calculate=new Calculate();
+			System.out.println(calculate.calc(2,4));
+	}
+
+}
---
+++ src/com/anveshon/Calculate.java
@@ -0,0 +1,8 @@
+package com.anveshon;
+
+public class Calculate {
+	public static int calc(int n1,int n2)
+	{
+		return n1*n2;
+	}
+}
